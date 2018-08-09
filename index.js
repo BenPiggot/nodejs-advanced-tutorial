346df038d012ee1adb6286d19b01a8d6111b7cc1
ben <wbpiggot@gmail.com>
2018-08-09 16:04:14 -0700

    adding clustering example

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
-const express = require('express');
-const app = express();
+const cluster = require('cluster');
 
-app.get('/', (req, res) => {
-  res.send('Hello!');
-})
+// is the file being executed in master mode?
+if (cluster.isMaster) {
+  // Cause index.js to be executed *again* but in child mode
+  cluster.fork();
+  cluster.fork();
+  cluster.fork();
+  cluster.fork();
+} 
+// I'm a child, I'm going to act like a server
+else {
+  const express = require('express');
+  const app = express();
 
-app.listen(3000)
+  function doWork(duration) {
+    const start = Date.now();
+
+    while (Date.now() - start < duration) { }
+  }
+
+  app.get('/', (req, res) => {
+    doWork(5000);
+    res.send('Hello!');
+  })
+
+  app.get('/fast', (req, res) => {
+    res.send('Fast!');
+  })
+
+  app.listen(3000)
+}
