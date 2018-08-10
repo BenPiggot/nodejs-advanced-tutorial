f0a09992da2fba12c36204a5aa652576b9897cb1
ben <wbpiggot@gmail.com>
2018-08-09 21:40:28 -0700

    workers and pm2

--- index.js
+++ index.js
@@ -1,32 +1,47 @@
-const cluster = require('cluster');
+process.env.UV_THREADPOOL_SIZE = 1;
+// const cluster = require('cluster');
+// const os = require('os');
 
-// is the file being executed in master mode?
-if (cluster.isMaster) {
-  // Cause index.js to be executed *again* but in child mode
-  cluster.fork();
-  cluster.fork();
-  cluster.fork();
-  cluster.fork();
-} 
-// I'm a child, I'm going to act like a server
-else {
-  const express = require('express');
-  const app = express();
+// // is the file being executed in master mode?
+// // if (cluster.isMaster) {
+// //   // Cause index.js to be executed *again* but in child mode
+// //   let counter = 0;
+// //   while (counter < os.cpus().length) {
+// //     cluster.fork();
+// //     counter++;
+// //   }
+// // } 
 
-  function doWork(duration) {
-    const start = Date.now();
+const express = require('express');
+const crypto = require('crypto');
+const Worker = require('webworker-threads').Worker;
+const app = express();
 
-    while (Date.now() - start < duration) { }
+app.get('/', (req, res) => {
+  // crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+  //   res.send('Hello!');
+  // })
+  const worker = new Worker(function() {
+    this.onmessage = function() {
+      let counter = 0;
+      while (counter < 1 * 1e9) {
+        counter++;
+      }
+      postMessage(counter);
+    }
+
+  });
+
+  worker.onmessage = function(message) {
+    console.log(message.data)
+    res.send('' + message.data)
   }
 
-  app.get('/', (req, res) => {
-    doWork(5000);
-    res.send('Hello!');
-  })
+  worker.postMessage();
+});
 
-  app.get('/fast', (req, res) => {
-    res.send('Fast!');
-  })
+app.get('/fast', (req, res) => {
+  res.send('Fast!');
+})
 
-  app.listen(3000)
-}
+app.listen(3000);
